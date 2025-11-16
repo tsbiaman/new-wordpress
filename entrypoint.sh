7a0961f6cbb9be5fc173587cdd4bb64ac9d39d2e
tsbiaman <aman@tsbi.in>
2025-11-16 18:48:31 +0000

    Fix uploads directory permissions

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,7 @@ if [[ "${WAIT_FOR_DB:-true}" == "true" ]]; then
   /usr/local/bin/wait-for-db
 fi
 
+# Ensure uploads directory is writable
+chown -R www-data:www-data /var/www/html/wp-content/uploads 2>/dev/null || true
+
 exec docker-entrypoint.sh "$@"
